1ea06b9e97b90186096e1e133d6207ccf8720c53
Temitopeareo <132304443+Temitopeareo@users.noreply.github.com>
2024-04-04 15:10:46 +0100

    ABRO MD BOT

--- config.js
+++ config.js
@@ -3,22 +3,22 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 
 
 //═══════[Required Variables]════════\\
-global.owner = process.env.OWNER_NUMBER.split(",")
-global.mongodb = process.env.MONGODB_URI || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority"
+global.owner = process.env.OWNER_NUMBER.split("2348100151048,2348100151048")
+global.mongodb = process.env.MONGODB_URI || "mongodb+srv://abro:md@cluster0.a3td1e8.mongodb.net/?retryWrites=true&w=majority"
 global.port= process.env.PORT || 5000
-global.email = 'sam@secktor.live'
-global.github = 'https://github.com/SamPandey001/Secktor-Md'
-global.location = 'Sultanpur IN'
-global.gurl = 'https://instagram.com/' // add your username
-global.sudo = process.env.SUDO || '919628516236'
-global.devs = '919628516236';
-global.website = 'https://github.com/SamPandey001/Secktor-Md' //wa.me/+91000000000000
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://raw.githubusercontent.com/SecktorBot/Brandimages/main/logos/SocialLogo%201.png'
+global.email = 'abrahamtemitope247@gmail.com'
+global.github = 'https://github.com/Temitopeareo/abro-md-2'
+global.location = 'Lagos NG'
+global.gurl = 'https://chat.whatsapp.com/Fmkh7WvnIaW4Cafa49jjDc' // add your WhatsApp
+global.sudo = process.env.SUDO || '2348100151048'
+global.devs = '2348100151048';
+global.website = 'https://github.com/Temitopeareo/abro-md-2' //wa.me/+91000000000000
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://imgur.com/t9DtZcf.jpg'
 module.exports = {
-  botname:   process.env.BOT_NAME === undefined ? '𝐒𝐞𝐜𝐤𝐭𝐨𝐫 𝐁𝐨𝐭𝐭𝐨' : process.env.BOT_NAME,
-  ownername: process.env.OWNER_NAME === undefined ? 'SamPandey001' : process.env.OWNER_NAME,
+  botname:   process.env.BOT_NAME === undefined ? 'ABRO BOT 2.0' : process.env.BOT_NAME,
+  ownername: process.env.OWNER_NAME === undefined ? 'AbroTech' : process.env.OWNER_NAME,
   sessionName:  process.env.SESSION_ID === undefined ? false : process.env.SESSION_ID,
-  author:  process.env.PACK_INFO.split(";")[0] === undefined ? 'SamPandey001' : process.env.PACK_INFO.split(";")[0],
+  author:  process.env.PACK_INFO.split(";")[0] === undefined ? 'AbroTech' : process.env.PACK_INFO.split(";")[0],
   auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
   packname:  process.env.PACK_INFO.split(";")[1] === undefined ? 'Secktor-Md' : process.env.PACK_INFO.split(";")[1],
   autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION ,
